8c27eb880608a958dfd1f3a405f5c1ce6f1691f7
Lendo <wesreedbusiness@gmail.com>
2023-07-29 23:26:21 -0500

    models

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reactionSchema');
+// Schema to create a course model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      minlength: 1,
+      maxlength: 280,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      //getter method to format the timestamp on query
+      get: createdAtVal => createdAtVal.toLocaleDateString()
+    },
+    username: {
+      type: String,
+      require: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
